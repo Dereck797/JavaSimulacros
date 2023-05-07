f88381569a927396c5c3f0d1767c8adef24ec39a
dereck797 <derecksebas7777@hotmail.com>
2023-05-06 20:43:21 -0500

    Tercer commit

--- src/Encapsulation/EncapsulationDifferentPackage/package1/A.java
+++ src/Encapsulation/EncapsulationDifferentPackage/package1/A.java
@@ -1,2 +1,8 @@
-package Encapsulation.EncapsulationDifferentPackage.package1;public class A {
+package Encapsulation.EncapsulationDifferentPackage.package1;
+
+public class A {
+  public int i1;
+  protected int i2;
+  int i3;
+  private int i4;
 }
--- src/Encapsulation/EncapsulationDifferentPackage/package2/ExampleAccess.java
+++ src/Encapsulation/EncapsulationDifferentPackage/package2/ExampleAccess.java
@@ -1,4 +1,13 @@
-package Encapsulation;
+package Encapsulation.EncapsulationDifferentPackage.package2;
+
+import Encapsulation.EncapsulationDifferentPackage.package1.A;
 
 public class ExampleAccess {
+  public static void main(String[] args) {
+    A obj = new A(); //Line 7
+    System.out.println(obj.i1); //Line 8
+    // System.out.println(obj.i2); //Line 9, no puede ser accedida debido a que una variable de tipo protected solo puede ser accedida por medio de la herencia
+    // System.out.println(obj.i3); //Line 10, no puede ser accedida debido a que una variable por default solo es publica para clases del mismo paquete
+    // System.out.println(obj.i4); //Line 11, no puede ser accedida debido a que una variable de tipo private no puede ser accedida directamente.
+  }
 }
